9a1a922eaee0a960a5c3d08b23e02e7fa3a8707e
Laura <laura.m.k.benz@gmail.com>
2019-05-24 11:22:28 +0200

    Add function: notes are rendered in input after leaving the page

--- src/recipes-detailed/Notes.js
+++ src/recipes-detailed/Notes.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { setLocal } from '../services'
+import React, { useState, useEffect } from 'react'
+import { getLocal, setLocal } from '../services'
 
 export default function Notes({ id }) {
-  const handleSave = (notes, id) => {
-    setLocal(`${id}Notes`, notes)
+  const [note, setNote] = useState(getLocal(`${id}Notes`) || '')
+
+  function handleChange(event) {
+    setNote(event.target.value)
   }
 
   function handleSubmit(event) {
     event.preventDefault()
-    const notes = event.target.notesInput.value
+    const notes = event.target.notesArea.value
     handleSave(notes, id)
   }
+  const handleSave = (notes, id) => {
+    setLocal(`${id}Notes`, notes)
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="notes">Here you may take some personal notes: </label>
-      <textarea id="notes" name="notesInput" type="text" placeholder="" />
+      <textarea
+        id="notes"
+        name="notesArea"
+        type="text"
+        placeholder=""
+        value={note}
+        onChange={handleChange}
+      />
       <button>Save notes</button>
     </form>
   )
