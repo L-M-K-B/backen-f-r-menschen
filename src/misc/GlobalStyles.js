de6686da45eab4ceb091e2ad078cd45321e15f04
Laura <laura.m.k.benz@gmail.com>
2019-05-25 23:22:26 +0200

    Adjust global styles

--- src/misc/GlobalStyles.js
+++ src/misc/GlobalStyles.js
@@ -1,15 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+
+@import url("https://fonts.googleapis.com/css?family=Domine&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
   * {
     box-sizing: border-box;
   }
 
   body {
     margin: 0;
-    padding: 20px;
-    font-family: sans-serif;
-    color: darkgoldenrod;
-    background: whitesmoke;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
   }
 `
+// für Überschriften: font-family: 'Domine', serif;
